9bfe9d94e3d4dc55292acfbd11a22da650ce9f20
DuBowei <dbw3559741123@qq.com>
2020-06-05 14:35:15 +0800

    add

--- *egg/app/controller/tools/down.js
+++ *egg/app/controller/tools/down.js
@@ -34,6 +34,35 @@ class DownController extends Controller {
     // console.log('begin=======2');
     // this.ctx.body = fs.createReadStream(filePath, _start, _end);
   }
+
+  /**
+   * @summary 下载
+   * @description nodejs版本大文件之断点下载 https://www.chrunlee.cn/article/nodejs-file-range-download.html
+   * @router get /api/superdown
+   * @response 200 uploadBaseResponse 创建成功
+   */
+  async superDown() {
+    const fs = require('fs');
+    const filePath = 'app/public/DBWJBQ.zip';
+    const range = this.ctx.request.headers.range;
+    console.log(`range = ${range}`)
+    if(range){
+      console.log('------->-----')
+      const [, start, end] = range.match(/(\d*)-(\d*)/);
+      const total = (await this.ctx.helper.promisify(fs.stat)(filePath)).size;
+      console.log(`fileSize = ${total}`);
+      // 处理请求头中范围参数不传的问题
+      start = start ? parseInt(start) : 0;
+      end = end ? parseInt(end) : total - 1;
+      this.ctx.body.statusCode = 206;
+      this.ctx.body.setHeader("Accept-Ranges", "bytes");
+      this.ctx.body.setHeader("Content-Range", `bytes ${start}-${end}/${total}`);
+      console.log(`start = ${start} end = ${end}`)
+      this.ctx.body = fs.createReadStream(filePath, start, end);
+    }else{
+      this.ctx.body = fs.createReadStream(filePath);
+    }
+  }
 }
 
 module.exports = DownController;
