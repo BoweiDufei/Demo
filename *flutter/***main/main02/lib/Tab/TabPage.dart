2e1a8896aabeb4e735bce5bf62b0e3a02c1e15eb
DuBowei <dbw3559741123@qq.com>
2020-06-22 15:14:25 +0800

    add

---
+++ *flutter/***main/main02/lib/Tab/TabPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: RaisedButton(onPressed: (){
+            Navigator.pushNamed(context, '/product');
+          }),
+        ),
+      ),
+    );
+  }
+}
