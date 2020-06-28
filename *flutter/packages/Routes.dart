6423a4956651cef86a09438ad09c77cb20116eda
DuBowei <dbw3559741123@qq.com>
2020-06-28 11:52:20 +0800

    add

---
+++ *flutter/packages/Routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../pages/MainTabPage.dart';
+
+final Map routes = {
+  '/': (context) => MainTabPage(),
+  // 使用 Navigator.pushNamed(context, '/productInfo',arguments: {"id":'我是上个界面传过来的id'});
+};
+
+var onGenerateRoute = (RouteSettings settings) {
+  final String name = settings.name;
+  final Function pageContentBuilder = routes[name];
+  if (pageContentBuilder != null) {
+    if (settings.arguments != null) {
+      final Route route = MaterialPageRoute(
+          builder: (context) =>
+              pageContentBuilder(context, arguments: settings.arguments));
+      return route;
+    } else {
+      final Route route =
+          MaterialPageRoute(builder: (context) => pageContentBuilder(context));
+      return route;
+    }
+  }
+};
