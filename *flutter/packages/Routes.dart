14d64ddd9ecd16b9c78f90626def9db32d9d4476
DuBowei <dbw3559741123@qq.com>
2020-06-28 16:33:56 +0800

    add

--- *flutter/packages/Routes.dart
+++ *flutter/packages/Routes.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import '../pages/MainTabPage.dart';
 import '../pages/LocationManager.dart';
 import '../pages/DioTest.dart';
+import '../pages/SwipperDemo.dart';
 
 final Map routes = {
   '/': (context) => MainTabPage(),
   '/location':(context) => LocationManager(),
   '/dio':(context) => DioTest(),
+  '/swipper':(context) => SwipperDemo(),
   // 使用 Navigator.pushNamed(context, '/productInfo',arguments: {"id":'我是上个界面传过来的id'});
 };
 
