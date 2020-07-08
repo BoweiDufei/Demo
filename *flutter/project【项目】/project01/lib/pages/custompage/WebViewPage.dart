37fcb5d00d89a9cec56b16805556100abfdab2e8
dbw <dbw3559741123@qq.com>
2020-07-08 21:46:03 +0800

    add

--- *flutter/project【项目】/project01/lib/pages/custompage/WebViewPage.dart
+++ *flutter/project【项目】/project01/lib/pages/custompage/WebViewPage.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebViewPage extends StatefulWidget {
-
-	final Map arguments;
-	WebViewPage({this.arguments});
+  final Map arguments;
+  WebViewPage({this.arguments});
 
   @override
   _WebViewPageState createState() => _WebViewPageState();
 }
 
 class _WebViewPageState extends State<WebViewPage> {
+  InAppWebViewController webView;
+  String url = "";
+  double progress = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +22,32 @@ class _WebViewPageState extends State<WebViewPage> {
       ),
       body: Container(
         color: Colors.deepPurple,
-        child: RaisedButton(onPressed: (){
-          print(widget.arguments);
-        }),
+        child: InAppWebView(
+          initialUrl: "https://www.baidu.com",
+          initialHeaders: {},
+          initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+            debuggingEnabled: true,
+          )),
+          onWebViewCreated: (InAppWebViewController controller) {
+            webView = controller;
+          },
+          onLoadStart: (InAppWebViewController controller, String url) {
+            setState(() {
+              this.url = url;
+            });
+          },
+          onLoadStop: (InAppWebViewController controller, String url) async {
+            setState(() {
+              this.url = url;
+            });
+          },
+          onProgressChanged: (InAppWebViewController controller, int progress) {
+            setState(() {
+              this.progress = progress / 100;
+            });
+          },
+        ),
       ),
     );
   }
