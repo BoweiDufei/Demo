9e5d2eee662452dd2a226d650d671d6229d451aa
DuBowei <dbw3559741123@qq.com>
2020-07-09 15:34:33 +0800

    add

--- *egg/app/schedule/update01.js
+++ *egg/app/schedule/update01.js
@@ -6,10 +6,10 @@ module.exports = {
   },
   async task(ctx) {
     const rootNums = [ '2', '51', '19' ];
-    for (let index = 0; index < rootNums.length; index++) {
-      const eleStr = rootNums[index];
-      const address = `http://web.anyv.net/index.php/categoryyuedu-${eleStr}-page-1`;
-      await ctx.service.tool.setPcPromise2(address);
-    }
+    // for (let index = 0; index < rootNums.length; index++) {
+    //   const eleStr = rootNums[index];
+    //   const address = `http://web.anyv.net/index.php/categoryyuedu-${eleStr}-page-1`;
+    //   await ctx.service.tool.setPcPromise2(address);
+    // }
   },
 };
