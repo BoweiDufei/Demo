a93806a938f635d46113cb02c53ba77c07d42d4b
DuBowei <dbw3559741123@qq.com>
2020-07-10 15:00:41 +0800

    add

--- *egg/app/schedule/news01.js
+++ *egg/app/schedule/news01.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   schedule: {
-    interval: '1h',
+    interval: '30m',
     type: 'all', // 指定所有的 worker 都需要执行
   },
   async task(ctx) {
@@ -10,6 +10,7 @@ module.exports = {
     if (hour < 4 || hour > 23) {
       return;
     }
+    // 央视网新闻
     const address = 'http://webpage.mbd.baidu.com/home?context={%22app_id%22:%221573407181922932%22}';
     await ctx.service.news.startGetBaijiaNewsMethod(address);
 
