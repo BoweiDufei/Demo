44ad54d4855cf5377a3ded9c1ca4a4acaa00aefe
DuBowei <dbw3559741123@qq.com>
2020-07-20 14:56:23 +0800

    add

--- *weChat/Uni-App/firstHX/pages/index/index.vue
+++ *weChat/Uni-App/firstHX/pages/index/index.vue
@@ -1,37 +1,77 @@
 <template>
 	<view class="container">
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
-		<button type="default" @click="btnClickMethod">我是按钮</button>
+		<view v-for="(item,index) in list" :key="index">
+		    <swipper v-if='item.type==="1"' :obj="item" @jump='jumpMethod'></swipper>
+			{{item.type}}
+		</view>
 	</view>
 </template>
 
 <script>
+	import swipper from '../../components/dbw-swipper/dbw-swipper.vue'
 	export default {
+		components:{
+			swipper
+		},
+		mounted() {
+			console.log('登录')
+		},
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				list: []
 			}
 		},
 		methods: {
+			onLoad(options){
+				console.log('onLoad')
+			},
 			btnClickMethod(){
 				uni.startPullDownRefresh()
 			},
-			onPullDownRefresh(){
-				console.log('onPullDownRefresh');
-				setTimeout(function() {
-					uni.stopPullDownRefresh()
-				}, 1000);
+			jumpMethod(jumpItem){
+				console.log(jumpItem)
+			},
+			async onPullDownRefresh(){
+				const resultList = [
+					{
+						"type":"1",
+						"interval":2000,
+						"duration":1000,
+						"autoplay":true,
+						"height":"500rpx",
+						"indicatorDots":true,
+						"elements":[
+							{
+								"pic":"https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+								"mode":"aspectFill",
+								"jump":"https://www.baidu.com"
+							},
+							{
+								"pic":"https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png",
+								"mode":"aspectFill",
+								"jump":"https://www.baidu.com"
+							},
+						]
+					},
+					{
+						"type":"2",
+						"elements":[]
+					},
+					{
+						"type":"3",
+						"elements":[]
+					}
+				];
+				this.list = [];
+				this.list = this.list.concat(resultList)
+				console.log(this.list)
+				uni.stopPullDownRefresh()
 			}
 		}
 	}
 </script>
 
 <style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
-	}
+	
 </style>
--- *weChat/Uni-App/firstHX/pages/second/second.vue
+++ *weChat/Uni-App/firstHX/pages/second/second.vue
@@ -1,45 +1,19 @@
 <template>
 	<view class="container">
-		<test msg='标题' @myEven='getNumber'></test>
+		
 		fff
-	  <view class="content">
-		  <image class="content-image" mode="aspectFill" src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2873034231,1191081182&fm=26&gp=0.jpg"></image>
-		  <view v-for="(item,index) in list" :key="index">
-		  <button type="primary" @click="btnClickMethod(index)">我是按钮</button>
-		  </view>
-		  {{this.$store.state.count}}
-		  <button @click="changeNumberMethod" type="default">改变vuex</button>
-	  </view>
 	</view>
 </template>
 
 <script>
-	import test from '../../components/dbw-test/dbw-test.vue'
-	import store from '../../store/store.js'
 	export default {
-		store,
-		components:{
-			test
-		},
 		data() {
 			return {
-				list:[
-					'从小宝',
-					'大栗子',
-					'妞儿'
-				]
+				
 			}
 		},
 		methods: {
-			btnClickMethod(index){
-				console.log(index);
-			},
-			getNumber(number){
-				console.log('父组件获取到',number)
-			},
-			changeNumberMethod(){
-				this.$store.commit('increseCount')
-			}
+			
 		}
 	}
 </script>
@@ -49,11 +23,5 @@
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
-		.content{
-			color: red;
-		}
-	}
-	.content-image{
-		width: 100%;
 	}
 </style>
