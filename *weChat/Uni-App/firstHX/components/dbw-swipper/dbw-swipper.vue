2a6195f8ac3a1f497827880ede95a0affecc6ecc
DuBowei <dbw3559741123@qq.com>
2020-07-20 14:56:23 +0800

    add

---
+++ *weChat/Uni-App/firstHX/components/dbw-swipper/dbw-swipper.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<swiper class="swiper" :style="{height: obj.height}" :indicator-dots="obj.indicatorDots" :autoplay="obj.autoplay" :interval="obj.interval" :duration="obj.duration">
+			
+			<swiper-item v-for="(item,index) in obj.elements">
+				<view class="swiper-item uni-bg-red" @click="swipperItemClick(item)">
+					<image class="swipperImg" :src="item.pic" :mode="item.mode"></image>
+				</view>
+			</swiper-item>
+			
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				swiperStyle:{},
+			};
+		},
+		props:{
+			obj:Object
+		},
+		methods:{
+			swipperItemClick(item){
+				this.$emit('jump', item.jump)
+			}
+		}
+	}
+</script>
+
+<style>
+	
+	.swipperImg{
+		width: 100%;
+	}
+
+</style>
