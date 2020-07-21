92b8741a3abdd2fb19d98ec29e149577489a0197
dbw <dbw3559741123@qq.com>
2020-07-21 21:21:59 +0800

    add

--- *nuxt_js/vue/web/src/main.js
+++ *nuxt_js/vue/web/src/main.js
@@ -3,23 +3,64 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 import First from './components/FirstComponent.vue'
-// import Second from './components/SecondComponse.vue'
 import Webview from './components/WebviewComponent.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import FirstHome from './components/FirstHome.vue'
+import FirstIntroduce from './components/FirstIntroduce.vue'
+import FirstAboutUs from './components/FirstAboutUs.vue'
+import FirstDown from './components/FirstDown.vue'
+import FirstContract from './components/FirstContract.vue'
+
+
+// 
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(ElementUI);
 
-const routes = [
-  { path: '/first', component: First },
-  { path: '/article/:id', component: Webview },
-  { path: '*', redirect: '/first' } /**重定向默认找不到路由进入 */
+const routes = [{
+		path: '/first',
+		component: First,
+		children: [
+			{
+				path: '/', // 默认路由
+				component: FirstHome
+			},
+			{
+				path: 'introduce', // 介绍 /
+				component: FirstIntroduce
+			},
+			{
+				path: 'about', // 关于 /
+				component: FirstAboutUs
+			},
+			{
+				path: 'contract', // 联系我们 /
+				component: FirstContract
+			},
+			{
+				path: 'down', // 下载 /
+				component: FirstDown
+			},
+		]
+	},
+	{
+		path: '/article/:id',
+		component: Webview
+	},
+	{
+		path: '*',
+		redirect: '/first'
+	} /**重定向默认找不到路由进入 */
 ]
 var router = new VueRouter({
-  routes
+	routes
 })
 
 new Vue({
-  render: h => h(App),
-  router
+	render: h => h(App),
+	router
 }).$mount('#app')
--- *nuxt_js/vue/web/vue.config.js
+++ *nuxt_js/vue/web/vue.config.js
@@ -1,18 +1,4 @@
 module.exports = {
     publicPath:"./",
-    outputDir:"dist",
-    assetsDir:"./static",
-    indexPath:"index.html",
-    filenameHashing:true,
-    pages:undefined,
-    lintOnSave:true,
-    runtimeCompiler:false,
-    transpileDependencies:[],
-    productionSourceMap:false,
-    crossorigin:undefined,
-    integrity:false,
-    devServer:{//代理
-        port:8080,
-        proxy:'http://127.0.0.1:8080'
-    }
+	lintOnSave: false
 }
