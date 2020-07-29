f280e366bb81d960f527908204c07bfc0be8632d
DuBowei <dbw3559741123@qq.com>
2020-07-29 16:25:17 +0800

    add

--- *egg/app/controller/admin/home.js
+++ *egg/app/controller/admin/home.js
@@ -3,17 +3,28 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const result = await this.ctx.model.Order.aggregate([
-      {
-        $lookup: {
-          from: 'product',
-          localField: 'orderId',
-          foreignField: 'orderId',
-          as: 'items',
-        },
-      },
-    ]);
-    this.ctx.body = await this.service.tool.getVerCode();
+    // const result = await this.ctx.model.Order.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: 'product',
+    //       localField: 'orderId',
+    //       foreignField: 'orderId',
+    //       as: 'items',
+    //     },
+    //   },
+    // ]);
+    // this.ctx.body = await this.service.tool.getVerCode();
+
+    const {app, query} = this.ctx;
+    // 给谁发, socket连接的id
+    const id = query.id;
+    const nsp = app.io.of('/');
+    if(nsp.sockets[id]){
+     // 通过id给指定socket连接发送消息
+      nsp.sockets[id].emit('res', 'hello http....');
+    }
+    this.ctx.body = "发送成功";
+    
   }
 }
 
--- *egg/app/router.js
+++ *egg/app/router.js
@@ -6,4 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.admin.home.index);
+  app.io.route('chat', app.io.controller.default.index);
 };
